52b7550db98ca363cf81bd745f14860b3e43f125
carolssun <carolinasuncastilho@gmail.com>
2025-09-09 13:55:38 -0300

    feat: added XCode project

---
+++ TesteImplementacao/TesteImplementacao/TesteImplementacaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteImplementacaoApp.swift
+//  TesteImplementacao
+//
+//  Created by Carolina Sun on 09/09/25.
+//
+
+import SwiftUI
+
+@main
+struct TesteImplementacaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TesteImplementacao/TesteImplementacaoTests/TesteImplementacaoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TesteImplementacaoTests.swift
+//  TesteImplementacaoTests
+//
+//  Created by Carolina Sun on 09/09/25.
+//
+
+import Testing
+@testable import TesteImplementacao
+
+struct TesteImplementacaoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
